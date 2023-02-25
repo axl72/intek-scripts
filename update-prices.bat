d651f2e14cd650918870af0e3fd51063375779c6
axl72 <axell.bernabel@unmsm.edu.pe>
2023-02-25 01:29:08 -0500

    actualizacion de precios v-alfa

---
+++ update-prices.bat
@@ -0,0 +1,4 @@
+python .\update_prices.py LP-30112022.xlsx ACT24012023.xlsx 24/01/2023
+python .\update_prices.py LP-24012023.xlsx nuevo.xlsx 01/02/2023
+python .\update_prices.py LP-01022023.xlsx ACT01032023.xlsx 01/03/2023
+pause
--- update_prices.py
+++ update_prices.py
@@ -1,42 +1,61 @@
-import openpyxl
 import sys
 import pandas as pd
-from datetime import date
-import os
 
-def update_date_df(dataframe, curret_date:str=date.today()) -> str:
-    datevar = curret_date
-    dataframe['Fecha de actualizacion'] = datevar
-    return datevar.__str__().replace('-', '.')
 
-def create_base_prices_list(path_file:str, margin:int=0.3):
-    """La lista de de precios inicial debe tener los siguientes campos: 'Codigo de origen', 'Descripcion Producto', 'Master Pack', 'PVP'.
-    El margen por defectdo es 30%, lo cual significa que la lista de precios base se generará en base a ese margen base."""
-    df = pd.read_excel(path_file)
-    df['30%'] = df['PVP']*(1-margin)
-    datestr = update_date_df(df, '11-11-2022')
-    df.to_excel('result.xlsx', index=False, sheet_name=datestr)
-  #  os.system("result.xlsx")
+def validar_formato(dataframe, formato_base: list[str]):
+    columnas = dataframe.columns.values
+    size = len(formato_base)
+    for i in range(size):
+        if formato_base[i] != columnas[i]:
+            message = f"El campo {columnas[i]} no coincide con {formato_base[i]}"
+            raise Exception(message)
+    print("Formato valido")
 
-def update_prices(base_list_prices_path:str, new_list_prices_path:str):
-    df_base = pd.read_excel(base_list_prices_path)
-    new_df = pd.read_excel(new_list_prices_path)
-    new_df['30%'] = new_df['PVP']*0.3
-    datastr=update_date_df(new_df, '01-02-2023')
-    updated_prices_list = pd.concat([df_base, new_df], ignore_index=True, axis=0).drop_duplicates(keep='last', subset=['COD. ORIGEN'])
-    updated_prices_list.to_excel('result.xlsx', index=False, sheet_name=datastr)
+
+def validar_codigos_origen(dataframe):
+    if len(dataframe[dataframe["COD. ORIGEN"].isnull()].index):
+        dataframe[dataframe["COD. ORIGEN"].isnull()].to_excel(
+            "log-sku-isnull.xlsx", index=False)
+        raise Exception("Las siguientes filas no tienen codigo de origen")
+    print("Todas las filas tienen codigo de origen")
+    df = dataframe.groupby(["COD. ORIGEN"], as_index=False).size()
+    if len(df[df['size'] != 1].index) > 0:
+        df[df['size'] != 1].to_excel("log-duplicated.sku.xlsx", index=False)
+        raise Exception("Existen SKU repetidos")
+    print("Validación de codigos de orige no repetidos exitosa")
+
+
+def combinar_listas(dataframe1, dataframe2):
+    nueva_lista_tmp = pd.concat([dataframe1, dataframe2])
+    nueva_lista_tmp = nueva_lista_tmp.drop_duplicates(
+        keep='last', subset=['COD. ORIGEN'])
+    nueva_lista_tmp = nueva_lista_tmp.sort_values(
+        "LINEA", ascending=False).reset_index()
+    return nueva_lista_tmp
+
 
 if __name__ == "__main__":
-    try:
-        prices_lists_path = sys.argv[1]
-        print(f"La lista de precios base es: {prices_lists_path}")
-        new_prices_path = sys.argv[2]
-        print(f"La lista de nuevos precios es: {new_prices_path}")
-        create_base_prices_list(prices_lists_path)
-        update_prices('result.xlsx', new_prices_path)
-        print("Prices List Update Successfully")
-        os.system('result.xlsx')
-    except PermissionError:
-        print(f"El archivo result.xlsx no puede ser accedido")
+    filename_lista_base = sys.argv[1]
+    filename_nuevos_precios = sys.argv[2]
+    fecha_actualizacion = sys.argv[3]
+    formato_base = ["COD. ORIGEN", "LINEA", "DESCRIPCION",
+                    "MASTER PACK", "PVP", "MARGEN 30%", "FECHA DE ACTUALIZACION"]
 
+    formato_nueva_lista = ["COD. ORIGEN", "LINEA",
+                           "DESCRIPCION", "MASTER PACK", "PVP"]
 
+    base = pd.read_excel(filename_lista_base, index_col=None)
+    validar_formato(base, formato_base)
+    validar_codigos_origen(base)
+    nueva_lista = pd.read_excel(filename_nuevos_precios)
+    validar_formato(nueva_lista, formato_nueva_lista)
+    validar_codigos_origen(nueva_lista)
+    nueva_lista["MARGEN 30%"] = nueva_lista["PVP"]*0.7
+    nueva_lista["FECHA DE ACTUALIZACION"] = fecha_actualizacion
+    result = combinar_listas(base, nueva_lista)
+    fecha_formateada = fecha_actualizacion.replace("/", "")
+    nombre_export = f'LP-{fecha_formateada}.xlsx'
+    print(nombre_export)
+    result = result.drop("index", axis=1)
+    result.to_excel(nombre_export, sheet_name=fecha_actualizacion.replace(
+        "/", "."), index=False)
